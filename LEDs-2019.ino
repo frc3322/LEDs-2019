1fabf031bee7f4e4d11bc12a7560f75ca4e878ed
Toby Wright <toby@tcw321.com>
2019-01-19 12:03:17 -0500

    Added scrolling like the arcade game 'stacker'

--- LEDs-2019.ino
+++ LEDs-2019.ino
@@ -18,30 +18,31 @@ int Sense(int InputNum) {
 }
 
 
-void Scroll(long col1, long col2, long col3, long col4, long col5) {
-//creates a scrolling effect for each induvidual line by moving each light value left
-//inputs are the five rightmost lights, bottom to top
-  for ( int i = 16; i > 0; i--) {
+void Scroll(long col1, long col2, long col3, long col4, long col5, int stay) {
+//Creates a scrolling effect for each induvidual line by moving each light value left
+//First five inputs are the five rightmost lights, bottom to top
+//Last input is the number of lights, right-to-left, that the scrolling ignores
+  for ( int i = 16-stay; i > 0; i--) {
     
     leds[i]=leds[i-1];
     }
 
-  for ( int i = 33; i > 17; i--) {
+  for ( int i = 33-stay; i > 17; i--) {
       
     leds[Sense(i)]=leds[Sense(i-1)];
     }
 
-  for ( int i = 50; i > 34; i--) {
+  for ( int i = 50-stay; i > 34; i--) {
       
     leds[i]=leds[i-1];
     }
 
-  for ( int i = 67; i > 51; i--) {
+  for ( int i = 67-stay; i > 51; i--) {
       
     leds[Sense(i)]=leds[Sense(i-1)];
     }
 
-  for ( int i = 84; i > 68; i--) {
+  for ( int i = 84-stay; i > 68; i--) {
       
     leds[i]=leds[i-1];
     }
@@ -50,18 +51,22 @@ void Scroll(long col1, long col2, long col3, long col4, long col5) {
   leds[Sense(17)] = col2;
   leds[34] = col3;
   leds[Sense(51)] = col4;
-  leds[68] = col5; FastLED.show(); delay(500);
+  leds[68] = col5; FastLED.show(); delay(250);
 }
 
-void SetupBlink() {
 
+
+
+void SetupBlink() {
+//stores the current set of lights that are on in another variable; only needs to be done once if blink() is used multiple times without changing the lights
   for ( int i = 0; i < 85; i++) {
     blinkBackup[i] = leds[i];
   }
 }
 
+
 void Blink() {
-  
+//flashes black for half a second. Use SetupBlink() first
   for ( int i = 0; i < 85; i++) {
     leds[i] = CRGB::Black;
   }
@@ -77,32 +82,41 @@ void Blink() {
 void loop() {
   // put your main code here, to run repeatedly:
 
-  Scroll(CRGB::Blue, CRGB::Black, CRGB::Blue, CRGB::Black, CRGB::Blue);
-  Scroll(CRGB::Blue, CRGB::Black, CRGB::Blue, CRGB::Black, CRGB::Blue);
-  Scroll(CRGB::Blue, CRGB::Blue, CRGB::Blue, CRGB::Blue, CRGB::Blue);
-  Scroll(CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black);
+  Scroll(CRGB::Blue, CRGB::Black, CRGB::Blue, CRGB::Black, CRGB::Blue, 0);
+  Scroll(CRGB::Blue, CRGB::Black, CRGB::Blue, CRGB::Black, CRGB::Blue, 0);
+  Scroll(CRGB::Blue, CRGB::Blue, CRGB::Blue, CRGB::Blue, CRGB::Blue, 0);
+  for ( int i = 0; i < 14; i++){
+    Scroll(CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, 0);
+    }
+  Scroll(CRGB::Blue, CRGB::Black, CRGB::Blue, CRGB::Black, CRGB::Blue, 4);
+  Scroll(CRGB::Blue, CRGB::Black, CRGB::Blue, CRGB::Black, CRGB::Blue, 4);
+  Scroll(CRGB::Blue, CRGB::Blue, CRGB::Blue, CRGB::Blue, CRGB::Blue, 4);
+  for ( int i = 0; i < 10; i++){
+    Scroll(CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, 4);
+    }
   
-  Scroll(CRGB::Blue, CRGB::Black, CRGB::Blue, CRGB::Black, CRGB::Blue);
-  Scroll(CRGB::Blue, CRGB::Black, CRGB::Blue, CRGB::Black, CRGB::Blue);
-  Scroll(CRGB::Blue, CRGB::Blue, CRGB::Blue, CRGB::Blue, CRGB::Blue);
-  Scroll(CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black);
-  
-  Scroll(CRGB::Blue, CRGB::Blue, CRGB::Blue, CRGB::Black, CRGB::Blue);
-  Scroll(CRGB::Blue, CRGB::Black, CRGB::Blue, CRGB::Black, CRGB::Blue);
-  Scroll(CRGB::Blue, CRGB::Black, CRGB::Blue, CRGB::Blue, CRGB::Blue);
-  Scroll(CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black);
+  Scroll(CRGB::Blue, CRGB::Blue, CRGB::Blue, CRGB::Black, CRGB::Blue, 8);
+  Scroll(CRGB::Blue, CRGB::Black, CRGB::Blue, CRGB::Black, CRGB::Blue, 8);
+  Scroll(CRGB::Blue, CRGB::Black, CRGB::Blue, CRGB::Blue, CRGB::Blue, 8);
+  for ( int i = 0; i < 6; i++){
+    Scroll(CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, 8);
+    }
 
-  Scroll(CRGB::Blue, CRGB::Blue, CRGB::Blue, CRGB::Black, CRGB::Blue);
-  Scroll(CRGB::Blue, CRGB::Black, CRGB::Blue, CRGB::Black, CRGB::Blue);
-  Scroll(CRGB::Blue, CRGB::Black, CRGB::Blue, CRGB::Blue, CRGB::Blue);
+  Scroll(CRGB::Blue, CRGB::Blue, CRGB::Blue, CRGB::Black, CRGB::Blue, 12);
+  Scroll(CRGB::Blue, CRGB::Black, CRGB::Blue, CRGB::Black, CRGB::Blue, 12);
+  Scroll(CRGB::Blue, CRGB::Black, CRGB::Blue, CRGB::Blue, CRGB::Blue, 12);
 
-
-  Scroll(CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black);
-  Scroll(CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black);
+  for ( int i = 0; i < 2; i++){
+    Scroll(CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, 12);
+    }
   delay(1000);
   SetupBlink();
   Blink();
   Blink();
   Blink();  
-  Scroll(CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black);
+  delay(1000);
+
+  for ( int i = 0; i < 17; i++){
+    Scroll(CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black, 0);
+    }
 }
